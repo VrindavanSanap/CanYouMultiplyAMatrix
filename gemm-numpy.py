7a5cbac5fa7fbff75e96c16402069f897aba924c
Vrindavan Sanap <vrindavansanap@gmail.com>
2023-03-04 03:22:29 +0530

    seperated torch and numpy implementations

---
+++ gemm-numpy.py
@@ -0,0 +1,34 @@
+# created by Vrindavan Sanap
+# all rights reserved
+
+import time
+import art
+import torch 
+
+N = 4096
+arr = []
+if __name__ == "__main__":
+    art.tprint("GPU go brrrr")
+    def matMul(N):
+        # N*N matrix
+        A = torch.randn(N, N, dtype=torch.float32)
+        # N*N matrix
+        B = torch.randn(N, N, dtype=torch.float32)
+        # flops=N^2 output cells with 2N compute each
+        flops = N*N*2*N
+
+        st = time.monotonic()
+        C = A@B
+        et = time.monotonic()
+        time_taken = (et-st)
+        g_flops = flops/1e9
+        g_flops_per_sec = g_flops/time_taken
+        return g_flops_per_sec
+    g_flops_per_sec_arr = []
+    for i in range(10):
+        g_flops_per_sec = matMul(N)
+        print(f"({i+1}/10) ,{g_flops_per_sec:.2f} GFlops/s")
+        g_flops_per_sec_arr.append(g_flops_per_sec)
+    g_flops_per_sec_arr = torch.Tensor(g_flops_per_sec_arr)
+    print(f"Mean: {g_flops_per_sec_arr.mean():.2f} GFlops/s")
+    print(f"Std: {g_flops_per_sec_arr.std():.2f} GFlops/s")
